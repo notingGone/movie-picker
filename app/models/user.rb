06ba0e6ea62b4e6635731d495ad45cc15cc19eb6
Elliott Woodward <elliottcwoodward@gmail.com>
2020-02-29 16:12:12 -0500

    Instantiate rails app, add bootstrap, and add user with devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :lists
+  # validates :email, presence: true, uniqueness: true, allow_nil: false, allow_blank: false
+end
